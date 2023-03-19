bcfb5df2ab853ece1f23d95645867eb2324f0b2f
Светлана Глухова <fire_fly_74@mail.ru>
2023-03-19 09:37:34 +0500

    Дз задачи 1,2,3,4

---
+++ task12.py
@@ -0,0 +1,6 @@
+s = int(input('Введите сумму чисел: '))
+p = int(input('Введите произведение чисел: '))
+for x in range(s):
+    for y in range(p):
+        if s == x + y and p == x * y:
+            print(x, y)
---
+++ task14.py
@@ -0,0 +1,5 @@
+n = int(input('Введите число n: '))
+i = 0
+while 2**i <= n:
+    print(2**i)
+    i += 1
